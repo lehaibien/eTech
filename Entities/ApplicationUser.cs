f85e0f73f25b8dcf93893b3721be4e4ec6f1a4be
lehaibien <lehaibien321@gmail.com>
2023-09-09 19:31:38 +0700

    Combine FirstName and LastName into Name of ApplicationUser

--- Entities/ApplicationUser.cs
+++ Entities/ApplicationUser.cs
@@ -4,8 +4,7 @@ namespace eTech.Entities
 {
   public class ApplicationUser : IdentityUser
   {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string Name { get; set; }
     public ICollection<Address> Addresses { get; set; } = new List<Address>();
     public ICollection<Order> Orders { get; set; }
     public ICollection<Rating> Ratings { get; set; }
