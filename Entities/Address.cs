2185c28b022e6c19bf4b70d2068564ac06fd7d3e
lehaibien <lehaibien321@gmail.com>
2023-09-09 19:31:01 +0700

    Combine FirstName and LastName into Name

--- Entities/Address.cs
+++ Entities/Address.cs
@@ -5,9 +5,8 @@ namespace eTech.Entities
   public class Address
   {
     [Key]
-    public long Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
     public string StreetAddress { get; set; }
     public string City { get; set; }
     public string Province { get; set; }
