3e946fe4ac2d354167b7c457e4a789c19b933384
lehaibien <lehaibien321@gmail.com>
2023-09-09 19:14:46 +0700

    Revert "Added entities"
    This reverts commit 00bd230758c21250aee05c919df2c6a8f50a882e.

--- Entities/CategoryImage.cs
+++
@@ -1,8 +0,0 @@
-namespace eTech.Entities
-{
-  public class CategoryImage : FileUpload
-  {
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
-  }
-}
--- Entities/ProductImage.cs
+++
@@ -1,8 +0,0 @@
-namespace eTech.Entities
-{
-  public class ProductImage : FileUpload
-  {
-    public int productId { get; set; }
-    public Product Product { get; set; }
-  }
-}
