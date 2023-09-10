aa7610e39390e7af839a3535575b480db195f53c
hoangnhii211203 <hoangnhii211203@gmail.com>
2023-09-10 17:11:22 +0700

    Conntect to Postgre

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using eTech.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace eTech.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Address> addresses { get; set; }
+        public DbSet<ApplicationUser> users { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<CategoryImage> categoryImages { get; set; }
+        public DbSet<FileUpload> fileUploads { get; set; }
+        public DbSet<Manufacturer> manufacturers { get; set; }
+        public DbSet<ManufacturerImage> manufacturerImages { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderItem> ordersItem { get; set; }
+        public DbSet<PaymentMethod> paymentMethods { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductImage> productImages { get; set; }
+        public DbSet<Rating> ratings { get; set; }
+        public DbSet<UserImage> userImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderItem>()
+            .HasKey(oi => new { oi.OrderId, oi.ProductId });
+
+        }
+    }
+}
