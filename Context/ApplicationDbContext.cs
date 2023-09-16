e794bf4ce79af465fe2c3e1d9f7a15f9465c2dbb
lehaibien <lehaibien321@gmail.com>
2023-09-16 18:37:07 +0700

    Change model in DbContext

--- Context/ApplicationDbContext.cs
+++ Context/ApplicationDbContext.cs
@@ -11,22 +11,17 @@ namespace eTech.Context
     public DbSet<Address> Addresses { get; set; }
     public DbSet<ApplicationUser> Users { get; set; }
     public DbSet<Category> Categories { get; set; }
-    public DbSet<CategoryImage> CategoryImages { get; set; }
     public DbSet<Manufacturer> Manufacturers { get; set; }
-    public DbSet<ManufacturerImage> ManufacturerImages { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
     public DbSet<PaymentMethod> PaymentMethods { get; set; }
     public DbSet<Product> Products { get; set; }
-    public DbSet<ProductImage> ProductImages { get; set; }
     public DbSet<Rating> Ratings { get; set; }
-    public DbSet<UserImage> UserImages { get; set; }
-
+    public DbSet<Image> Images { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
       modelBuilder.Entity<OrderItem>()
-      .HasKey(oi => new { oi.OrderId, oi.ProductId });
-
+        .HasKey(oi => new { oi.OrderId, oi.ProductId });
     }
   }
 }
