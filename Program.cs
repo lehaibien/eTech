4127e104e54803e4186ae6ea823de9ffcda3291c
lehaibien <lehaibien321@gmail.com>
2023-09-16 18:38:18 +0700

    Add manufacturer service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using eTech.Context;
+using eTech.Services;
+using eTech.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,19 +12,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Add identity service
+// builder.Services.AddIdentity<IdentityUser, IdentityRole>();
 
+// Db Context
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("eTech"))
 );
+// Dependency Injection
+builder.Services.AddScoped<IManufacturerService, ManufacturerService>();
+
+// app
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
