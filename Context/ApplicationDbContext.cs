ffefb28c7b868e03f127cab1d595e127a590bdab
lehaibien <lehaibien321@gmail.com>
2023-09-18 10:04:29 +0700

    Change dbcontext to match entities

--- Context/ApplicationDbContext.cs
+++ Context/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using eTech.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace eTech.Context
 {
@@ -9,12 +8,12 @@ namespace eTech.Context
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<Address> Addresses { get; set; }
-    public DbSet<ApplicationUser> Users { get; set; }
+    // public DbSet<ApplicationUser> Users { get; set; }
     public DbSet<Category> Categories { get; set; }
-    public DbSet<Manufacturer> Manufacturers { get; set; }
+    public DbSet<Brand> Brands { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
-    public DbSet<PaymentMethod> PaymentMethods { get; set; }
+    public DbSet<Payment> Payments { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Rating> Ratings { get; set; }
     public DbSet<Image> Images { get; set; }
@@ -22,6 +21,10 @@ namespace eTech.Context
     {
       modelBuilder.Entity<OrderItem>()
         .HasKey(oi => new { oi.OrderId, oi.ProductId });
+      modelBuilder.Entity<Category>()
+        .HasMany(cat => cat.Products).WithOne(p => p.Category);
+      modelBuilder.Entity<Brand>()
+        .HasMany(b => b.Products).WithOne(p => p.Brand);
     }
   }
 }
